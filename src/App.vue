c7d51bb7c41d18fbfa17f17d4aa5fa15bd2993fa
AldoMichael <73496845+AldoMichael@users.noreply.github.com>
2025-03-23 01:22:08 +0300

    Correction de checkBox, correction ded trie et reorganisation du liste et aussi optimisation du code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
+  <button @click="triTodoList">Reorganise</button>
   <form action="" @submit.prevent="ajoutList">
   <p>Eto ilay test page</p>
   <input type="text" v-model="listTodo.title">{{ listTodo.title }}
   <button>Click</button><hr>
-  <button @click="triTodoList">Reorganise</button>
   <div v-if="todoLists.length > 0">
     <ul v-for="(todoList, index) in todoLists" :key="index">
       <li>
-        <input type="checkbox" v-model="checkList[index]" :id="'check-' + index">
-            <span :class="{barre: checkList[index]}">{{todoList}}</span>
+        <input type="checkbox" v-model="todoList.completed" :id="'check-' + index" @click="Completed(index)">
+            <span :class="{barre: todoList.completed}">{{todoList}}</span>
         <button @click="suppList(index)">Supprimer</button>
       </li>
-      {{ checkList[index] }}
     </ul>
   </div>
   <div v-else>
@@ -23,35 +22,38 @@
 <script setup>
 import { ref } from 'vue'
 
-const listTodo = ref([{title:'', completed: false, date: new Date()}])
-const checkList = ref([])
-const todoLists = ref([{title:'', completed: false, date: new Date()}])
+const listTodo = ref([{title:'', completed: false, date: new Date()}]);
+//const checkList = ref([]);
+const todoLists = ref([]);
 
 const ajoutList = () =>{
   if(listTodo.value.title){
-    alert(listTodo.value.title, listTodo.value.title)
-    console.log('listTodo---->', listTodo.value)
-    todoLists.value.push({title: listTodo.value.title, completed : checkList.value, date: new Date()})
-    console.log('todoLists---->', todoLists.value)
+    alert(listTodo.value.title, listTodo.value.title);
+    todoLists.value.push({
+      title: listTodo.value.title, 
+      completed : false, 
+      date: new Date()});
     listTodo.value.title = "";
   }
   else{
-    alert("Input list vide")
+    alert("Input list vide");
   }
 }
 
 const suppList = (index) =>{
   if(confirm("Tena ho fafana ny Lisy " + index))
   {
-    //todoLists.index = todoLists.index.filter(i => i !== index)
     todoLists.value.splice(index, 1);
   }
 }
 
-const triTodoList = () => {
-  todoLists.value.sort((a, b) => a > b ? 1 : -1)
-  checkList.value.sort((a, b) => a - b ? 1 : -1)
+const Completed = (index) => {
+  todoLists.value[index].completed = !todoLists.value[index].completed;
 }
+
+const triTodoList = () => {
+  todoLists.value.sort((a, b) => a.completed - b.completed);
+  }
 
 </script>
 
